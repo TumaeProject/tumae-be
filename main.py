b3c1f41ce9334d18c30ade9ca411db53d059ad63
hyoeun02 <hyoeun.star@gmail.com>
2025-11-20 13:46:08 +0900

    add student_age main.py

--- main.py
+++ main.py
@@ -151,6 +151,7 @@ class StudentDetailsRequest(BaseModel):
     preferred_price_min: int
     preferred_price_max: int
     student_skill_levels: List[int]
+    student_age: int
 
 # --- 학생 검색 응답 ---
 class StudentListResponse(BaseModel):
